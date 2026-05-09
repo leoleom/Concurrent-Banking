a3ce3fc34e93d9d6f46df777c95dde7f2098155c
rykonnen <lqblancaflor@up.edu.ph>
2026-05-10 02:05:20 +0800

    feat: implemented initial execute_transactions

---
+++ src/transaction.c
@@ -0,0 +1,110 @@
+#include "transaction.h"
+#include "bank.h"
+#include "buffer_pool.h"
+#include "timer.h"
+#include "lock_mgr.h"
+#include "metrics.h"
+#include "utils.h"
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+
+/* run transactions */
+void* execute_transaction(void* arg) {
+    Transaction* tx = (Transaction*)arg;
+    
+    // Wait until scheduled start time
+    wait_until_tick(tx->start_tick);
+    
+    tx->actual_start = global_tick;
+    
+    for (int i = 0; i < tx->num_ops; i++) {
+        Operation* op = &tx->ops[i];
+        
+        int tick_before = global_tick;
+        
+        switch (op->type) {
+            case OP_DEPOSIT:
+                deposit(op->account_id, op->amount_centavos);
+                break;
+                
+            case OP_WITHDRAW:
+                if (!withdraw(op->account_id, op->amount_centavos)) {
+                    // Insufficient funds - abort transaction
+                    tx->status = TX_ABORTED;
+                    return NULL;
+                }
+                break;
+                
+            case OP_TRANSFER:
+                if (!transfer(op->account_id, op->target_account,
+                              op->amount_centavos)) {
+                    tx->status = TX_ABORTED;
+                    return NULL;
+                }
+                break;
+                
+            case OP_BALANCE:
+                int balance = get_balance(op->account_id);
+                printf("T%d: Account %d balance = PHP %d.%02d\n", 
+                       tx->tx_id, op->account_id, 
+                       balance / 100, balance % 100);
+                break;
+        }
+        
+        tx->wait_ticks += (global_tick - tick_before);
+    }
+    
+    tx->actual_end = global_tick;
+    tx->status = TX_COMMITTED;
+    return NULL;
+}
+
+/* trace parser */
+int parse_operation(const char *line, Operation *op)
+{
+    char type[16];
+    if (sscanf(line, "%15s", type) != 1)
+        return -1;
+    if (type[0] == '#')
+        return -1;
+
+    if (strcmp(type, "DEPOSIT") == 0)
+    {
+        op->type = OP_DEPOSIT;
+        op->target_account = -1;
+        return (sscanf(line, "%*s %d %" SCNd64,
+                       &op->account_id, &op->amount_centavos) == 2)
+                   ? 0
+                   : -1;
+    }
+    if (strcmp(type, "WITHDRAW") == 0)
+    {
+        op->type = OP_WITHDRAW;
+        op->target_account = -1;
+        return (sscanf(line, "%*s %d %" SCNd64,
+                       &op->account_id, &op->amount_centavos) == 2)
+                   ? 0
+                   : -1;
+    }
+    if (strcmp(type, "BALANCE") == 0)
+    {
+        op->type = OP_BALANCE;
+        op->target_account = -1;
+        op->amount_centavos = 0;
+        return (sscanf(line, "%*s %d", &op->account_id) == 1) ? 0 : -1;
+    }
+    if (strcmp(type, "TRANSFER") == 0)
+    {
+        op->type = OP_TRANSFER;
+        return (sscanf(line, "%*s %d %d %" SCNd64,
+                       &op->account_id,
+                       &op->target_account,
+                       &op->amount_centavos) == 3)
+                   ? 0
+                   : -1;
+    }
+
+    fprintf(stderr, "[parser] unknown operation: %s\n", type);
+    return -1;
+}
