e9ad053449c181e8a54f5b9da9426b273b4fd670
rykonnen <lqblancaflor@up.edu.ph>
2026-05-17 00:55:24 +0800

    refactor: removed artifact and added .gitignore

--- src/utils.c
+++ src/utils.c
@@ -2,17 +2,11 @@
 #include <stdio.h>
 #include <time.h>
 
+
 void fmt_centavos(int centavos, char *buf, int bufsz)
 {
-    // null check and invalid size
-    if (!buf || bufsz <= 0)
-        return;
-
     int negative = (centavos < 0);
-
-    if (negative)
-        centavos = -centavos;
-
+    if (negative) centavos = -centavos;
     snprintf(buf, (size_t)bufsz, "%sPHP %d.%02d",
              negative ? "-" : "",
              centavos / 100,
